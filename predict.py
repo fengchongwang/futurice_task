5f977408ae21ccd3ea296ae113fd0f9b5f54cfa1
fengchong <fengchongwang@gmail.com>
2018-11-12 01:02:53 +0100

    futurice task

--- predict.py
+++ predict.py
@@ -8,30 +8,47 @@ import pickle
 from sklearn.externals import joblib
 from flask import Flask
 
-app = Flask(__name__)
-
 transformer = pickle.load(open("transformer.p", "rb"))
 model = joblib.load('model_bayesian.joblib')
 
 app = Flask(__name__)
 api = Api(app)
 
+@app.route('/my400')
+def my400(msg = None):
+    code = 400
+    if msg is None:
+        msg = '400 Bad request'
+    else:
+        msg = '400 Bad request - ' + msg
+    return msg, code
+
 class HelloWorld(Resource):
+    
     def post(self):
         json_data = request.get_json(force=True)
-        crime_rate = json_data['crime_rate']
+        json_names = list(json_data.keys())
+        if 'crime_rate' not in json_names or 'avg_number_of_rooms' not in json_names or 'distance_to_employment_centers' not in json_names or 'property_tax_rate' not in json_names or 'pupil_teacher_ratio' not in json_names:
+            return my400()
+        crime_rate = json_data['crime_rate']        
         avg_number_of_rooms = json_data['avg_number_of_rooms']
         distance_to_employment_centers = json_data['distance_to_employment_centers']
         property_tax_rate = json_data['property_tax_rate']
         pupil_teacher_ratio = json_data['pupil_teacher_ratio']
-        if crime_rate is None or avg_number_of_rooms is None \
-                or distance_to_employment_centers is None \
-                or property_tax_rate is None or pupil_teacher_ratio is None:
-            return 0
-        else:
-            names_col = ["crime_rate", "avg_number_of_rooms", "distance_to_employment_centers",
+        if crime_rate > 1.0 or crime_rate < 0:
+            return my400("crime_rate should be between 0 and 1")        
+        elif avg_number_of_rooms > 11 or avg_number_of_rooms < 0:
+            return my400("avg_number_of_rooms should be between 0 and 11")
+        elif distance_to_employment_centers < 0 or distance_to_employment_centers > 15:
+            return my400("distance_to_employment_centers should be between 0 and 15")
+        elif property_tax_rate < 100 or property_tax_rate > 1000:
+            return my400("property_tax_rate should be between 100 and 1000")
+        elif pupil_teacher_ratio < 0 or pupil_teacher_ratio > 30:
+            return my400("pupil_teacher_ratio should be between 0 and 30")
+        names_col = ["crime_rate", "avg_number_of_rooms", "distance_to_employment_centers",
                          "property_tax_rate", "pupil_teacher_ratio"]
-            input_data = pd.DataFrame([[crime_rate,avg_number_of_rooms,distance_to_employment_centers,property_tax_rate,pupil_teacher_ratio]], columns=names_col)
+        input_data = pd.DataFrame([[crime_rate,avg_number_of_rooms,distance_to_employment_centers,property_tax_rate,pupil_teacher_ratio]], columns=names_col)
+            
         if isinstance(input_data, pd.core.series.Series):
             input_data = input_data.to_frame().transpose()
         
@@ -42,4 +59,4 @@ class HelloWorld(Resource):
 api.add_resource(HelloWorld, '/predict')
 
 if __name__ == '__main__':
-    app.run(debug=True, use_reloader=True)
+    app.run(debug=True, host="0.0.0.0", port=5000)
